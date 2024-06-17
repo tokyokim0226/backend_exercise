185e50190427a9e5ff95b9abb1246c218019e310
Seongmoon Kim <tylerk0226@naver.com>
2024-06-17 23:32:22 +0900

    creating an app inside a project, render a view

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -30,14 +30,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+INSTALLED_APPS = [ 
+    'django.contrib.admin', 
+    'django.contrib.auth',  
+    'django.contrib.contenttypes', 
+    'django.contrib.sessions', 
+    'django.contrib.messages', 
+    'django.contrib.staticfiles', 
+    'demoapp', 
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
